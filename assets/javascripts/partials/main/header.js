63f80ae1b4e525a2bf549bcae34a6b07ec4f040c
tatia-kom <komarovats93@gmail.com>
2020-12-28 01:56:44 +0300

    add responsive

--- assets/javascripts/partials/main/header.js
+++ assets/javascripts/partials/main/header.js
@@ -9,14 +9,36 @@ $(document).ready(function () {
         }
     });
 
-    $('.header-menu__link--opening > a').mouseover(function(e) {
-        $(this).parents('.header-menu__link').addClass('header-menu__link--active');
-        $(this).next('.header-menu__submenu').addClass('header-menu__submenu--active');
-    });
+    if ($(window).width() > 767) {
 
-    $('.header-menu__link--opening').mouseleave(function(e) {
-        $(this).find('.header-menu__submenu').removeClass('header-menu__submenu--active');
-        $(this).removeClass('header-menu__link--active');
-    });
+        $('.header-menu__link--opening > a').mouseover(function (e) {
+            $(this).parents('.header-menu__link').addClass('header-menu__link--active');
+            $(this).next('.header-menu__submenu').addClass('header-menu__submenu--active');
+        });
+
+        $('.header-menu__link--opening').mouseleave(function (e) {
+            $(this).find('.header-menu__submenu').removeClass('header-menu__submenu--active');
+            $(this).removeClass('header-menu__link--active');
+        });
+    }
+    else {
+        $('.header-menu__link--opening > a').click(function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $(this).parents('.header-menu__link').addClass('header-menu__link--active');
+            $(this).next('.header-menu__submenu').addClass('header-menu__submenu--active');
+        });
+
+        $('body').click(function() {
+            $('.header-menu__link').removeClass('header-menu__link--active');
+            $('.header-menu__submenu').removeClass('header-menu__submenu--active');
+        });
+
+        $('.header__burger').click(function(e) {
+            e.preventDefault();
+            $(this).toggleClass('header__burger--active');
+            $('.header-menu').slideToggle();
+        });
+    }
 
 });
